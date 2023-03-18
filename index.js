5a09558c1395d646aedbfd89d5ee93aeb3095a7e
Majid Hojati <Majid Hojati@eag.environics.ca>
2023-03-18 01:06:15 -0400

    work on progress

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Assume add.wasm file exists that contains a single function adding 2 provided arguments
-const fs = require('fs');
-
-const wasmBuffer = fs.readFileSync('aa.wasm');
-WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
-  // Exported function live under instance.exports
-  const { add } = wasmModule.instance.exports;
-  const sum = add(5, 6);
-  console.log(sum); // Outputs: 11
-});
+import WEBDGGRID  from './dist/webdggrid.js'
+const module = await WEBDGGRID();
+debugger
--- src/test.cpp
+++ src/test.cpp
@@ -13,35 +13,63 @@ int main()
   return cells;
 }
 
-// extern "C"
-// {
-// a =1 ; b=2;c=3;d=4
-// 0,0,0,3,10,'HEXAGON','ISEA'
-// EMSCRIPTEN_KEEPALIVE
+
+// a=new Module.DoubleVectorVector()
+// b= new Module.DoubleVector()
+// b.push_back(0)
+// b.push_back(0)
+// a.push_back(b)
+// Module.DgGEO_to_SEQNUM(0,0,0,3,10,'HEXAGON','ISEA',a)
 std::vector<uint64_t> DgGEO_to_SEQNUM(
-    long double pole_lon_deg,
-    long double pole_lat_deg,
-    long double azimuth_deg,
+    double pole_lon_deg,
+    double pole_lat_deg,
+    double azimuth_deg,
     unsigned int aperture,
-    int res)
+    int res,
+    std::string topology,
+    std::string projection,
+    std::vector<std::vector<double>> coordinates_deg)
 {
 
-  printf("Decimals: %d %s", pole_lon_deg);
+  printf("Decimals: %d ", pole_lon_deg);
 
-  dglib::Transformer dgt(pole_lon_deg, pole_lat_deg, azimuth_deg, aperture, res, "topology", "projection");
-  std::string topology;
-  std::string projection;
-  std::vector<long double> lon_deg;
-  std::vector<long double> lat_deg;
+  dglib::Transformer dgt(pole_lon_deg, pole_lat_deg, azimuth_deg, aperture, res, topology, projection);
+
   std::vector<uint64_t> seqnum;
-  for (unsigned int i = 0; i < lon_deg.size(); i++)
+  for (unsigned int i = 0; i < coordinates_deg.size(); i++)
   {
-    auto in = dgt.inGEO(lon_deg.at(i), lat_deg.at(i));
+    std::vector<double> point =coordinates_deg.at(i);
+    auto in = dgt.inGEO(point.at(0), point.at(1));
     dgt.outSEQNUM(in, seqnum.at(i));
   }
   return seqnum;
 }
-// }
+
+long double testuint(double pole_lon_deg)
+{
+  //  return 2^64;
+  long double newvalue = static_cast<long double>(pole_lon_deg);
+  return newvalue;
+  //  std::transform(intVec.begin(), intVec.end(), doubleVec.begin(), [](int x) { return (double)x;});
+}
+
+std::vector<int> test2(std::vector<double> pole_lon_deg)
+{
+  //  return 2^64;
+  // long double newvalue = static_cast<long double>(pole_lon_deg);
+  std::vector<int> fl;
+
+  std::transform(pole_lon_deg.begin(), pole_lon_deg.end(), std::back_inserter(fl), [](double x)
+                 { return static_cast<int>(x); });
+  return fl;
+  //  std::transform(intVec.begin(), intVec.end(), doubleVec.begin(), [](int x) { return (double)x;});
+}
+
+std::vector<double> test3(double pole_lon_deg)
+{
+  std::vector<double> v(10, 1.5);
+  return v;
+}
 
 std::string getExceptionMessage(intptr_t exceptionPtr)
 {
@@ -53,7 +81,14 @@ EMSCRIPTEN_BINDINGS(my_module)
   register_vector<std::vector<int>>("IntegerVectorVector");
   register_vector<long double>("LongDoubleVector");
   register_vector<std::vector<long double>>("LongDoubleVectorVector");
+  register_vector<std::vector<double>>("DoubleVectorVector");
   register_vector<uint64_t>("BigIntegerVector");
+  register_vector<int>("IntVector");
+  register_vector<double>("DoubleVector");
   emscripten::function("getExceptionMessage", &getExceptionMessage);
   emscripten::function("DgGEO_to_SEQNUM", &DgGEO_to_SEQNUM);
+  emscripten::function("testuint", &testuint);
+  emscripten::function("test2", &test2);
+  emscripten::function("test3", &test3);
+  // emscripten::function("test4", &test4);
 }
