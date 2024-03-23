0a7d570947be5121abca37685c38beef763265e7
Majid <majid@ready.net>
2024-03-23 11:41:29 -0400

    added docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,24 +2,25 @@ import { defineConfig } from 'vitepress'
 import typedocSidebar from '../api/typedoc-sidebar.json';
 
 
-const sidebarModified= typedocSidebar.map((groups)=>{
-  const items= groups.items.map((item)=>{ 
+const sidebarModified = typedocSidebar.map((groups) => {
+  const items = groups.items.map((item) => {
 
-    return {...item, link:item.link.replace('/docs','')}
+    return { ...item, link: item.link.replace('/docs', '') }
   })
-  return {...groups, items}
+  return { ...groups, items }
 })
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "WebDggrid",
   description: "A webassembely version of DGGRID",
-  base:"/webDggrid/",
+  base: "/webDggrid/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'API', link: '/api/' }
+      { text: 'API', link: '/api/' },
+      { text: 'Demo', link: 'example.html' }
     ],
 
     sidebar: [
